9b913346174cb5db9763096de066764bf16f394c
Samanvya Tripathi <samanu09@gmail.com>
2023-08-05 17:59:26 -0400

    add upload functionality

--- controllers/file_service/file-controller.js
+++ controllers/file_service/file-controller.js
@@ -1,90 +1,20 @@
-import { getDownloadURL, uploadBytesResumable, deleteObject, ref } from "firebase/storage";
-import generateUniqueFileName from "./filename_hasher.js";
+import { deleteObject, ref } from "firebase/storage";
 import firebaseInit from "./firebase_init.js";
+
 
 const folderName = 'mapverse_images/';
 // Initializes our firebase storage object which will be used for file operations
-const fireBaseStorage = firebaseInit();
-// console.log(fireBaseStorage);
-const firebaseStorageRef = ref(fireBaseStorage)
-// console.log(firebaseStorageRef)
+const multer = firebaseInit();
 
 export default (app) => {
-    app.post('/api/files/upload', uploadFile);
-    // app.post('/api/files/download', downloadFile);
+    app.post('/api/files/upload', multer.single('file'),uploadFile);
+    app.post('/api/files/multipleupload', multer.array())
     app.post('/api/files/delete', deleteFile);
 }
 
 const uploadFile = (req, res) => {
-    console.log(req);
-    const fileName = req.body.fileName;
-    const fileContent = req.body.imgData;
-    console.log(fileName, fileContent);
-    const newFileName = generateUniqueFileName(fileName);
-    const uploadFileRef = ref(fireBaseStorage,folderName+newFileName);
-    const uploadTask = uploadBytesResumable(uploadFileRef, fileContent);
-
-    uploadTask.on('state_changed', 
-    (snapshot) => {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-        case 'paused':
-            console.log('Upload is paused');
-            break;
-        case 'running':
-            console.log('Upload is running');
-            break;
-        }
-    }, 
-    (error) => {
-        // Handle unsuccessful uploads
-        console.log('File upload failed: ', error)
-    }, 
-    () => {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        console.log('File available at', downloadURL);
-        const response = {
-            success: true,
-            downloadUrl: downloadURL
-          };
-        res.json(response);
-        });
-    });
-
+    res.status(201).json(req.file);
 }
-
-// const downloadFile = (req, res) => {
-//     const fileURL = req.params.url;
-//     const tempFileSplit = fileURL.split('/')
-//     const fileName = tempFileSplit[tempFileSplit.length - 1];
-
-//     getDownloadURL(ref(firebaseStorageRef, folderName+fileName))
-//   .then((url) => {
-//     // `url` is the download URL for 'images/stars.jpg'
-
-//     // This can be downloaded directly:
-//     const xhr = new XMLHttpRequest();
-//     xhr.responseType = 'blob';
-//     xhr.onload = (event) => {
-//       const blob = xhr.response;
-//     };
-//     xhr.open('GET', url);
-//     xhr.send();
-
-//     // Or inserted into an <img> element
-//     const img = document.getElementById('myimg');
-//     img.setAttribute('src', url);
-//   })
-//   .catch((error) => {
-//     console.log("The file didn't download: ", error)
-//   });
-
-// }
 
 const deleteFile = (req, res) => {
     const fileLink = req.body.url;
--- controllers/file_service/firebase_init.js
+++ controllers/file_service/firebase_init.js
@@ -1,23 +1,35 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import FirebaseStorage from "multer-firebase-storage";
+import Multer from "multer";
+import "dotenv/config";
 
 function firebaseInit() {
-  let fireBaseStorage = null;
+  // let fireBaseStorage = null;
   // TODO: Replace the following with your app's Firebase project configuration
   // See: https://firebase.google.com/docs/web/learn-more#config-object
-  console.log(process.env.FIREBASE_STORAGE_URL) // CHECK THIS
-  const firebaseConfig = {
-    storageBucket: 'gs://mapverse-e16e8.appspot.com'
-    // storageBucket: process.env.FIREBASE_STORAGE_URL
-  };
+  const multer = Multer({
+    storage: FirebaseStorage({
+      bucketName: process.env.FIREBASE_STORAGE_URL,
+      directoryPath: 'mapverse_images',
+      credentials: {
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n") : undefined,
+        projectId: process.env.FIREBASE_PROJECT_ID
+      },
+      public: true,
+      unique: true
+    })
+  })
 
   // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
+  // const firebaseApp = initializeApp(firebaseConfig);
 
 
-  // Initialize Cloud Storage and get a reference to the service
-  fireBaseStorage = getStorage(firebaseApp);
-  return fireBaseStorage;
+  // // Initialize Cloud Storage and get a reference to the service
+  // fireBaseStorage = getStorage(firebaseApp);
+  // return fireBaseStorage;
+  return multer;
 }
 
 export default firebaseInit;
