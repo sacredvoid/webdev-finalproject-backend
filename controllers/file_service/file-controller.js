4c27b6982ab5962ce4489415b08755996d67413b
Samanvya Tripathi <samanu09@gmail.com>
2023-08-07 18:54:33 -0400

    (WIP) delete function

--- controllers/file_service/file-controller.js
+++ controllers/file_service/file-controller.js
@@ -1,26 +1,32 @@
-import { deleteObject, ref } from "firebase/storage";
+import { deleteObject, getStorage, ref } from "firebase/storage";
 import firebaseInit from "./firebase_init.js";
 
 
 const folderName = 'mapverse_images/';
 // Initializes our firebase storage object which will be used for file operations
-const multer = firebaseInit();
-
+const { multer, fbStorage } = firebaseInit();
 export default (app) => {
-    app.post('/api/files/upload', multer.single('file'),uploadFile);
+    app.post('/api/files/upload', multer.single('file'), uploadFile);
     app.post('/api/files/multipleupload', multer.array())
     app.post('/api/files/delete', deleteFile);
 }
 
 const uploadFile = (req, res) => {
+    // console.log(req.file);
     res.status(201).json(req.file);
 }
 
+function extractFileName(link) {
+  let tempSplit = link.split("%2F");
+  let fileName = tempSplit[1].split("?")[0];
+  return fileName;
+}
+
 const deleteFile = (req, res) => {
-    const fileLink = req.body.url;
-    const tempFileSplit = fileLink.split('/');
-    const fileName = tempFileSplit[tempFileSplit.length - 1];
-    const fileRef = ref(fireBaseStorage, folderName+fileName);
+    const fileLink = req.body.media_url;
+    const fileName = extractFileName(fileLink);
+    const fileRef = ref(fbStorage, folderName+fileName);
+    console.log("\n\nFILE REF!:::", fileRef)
     deleteObject(fileRef).then(() => {
         res.sendStatus(200);
       }).catch((error) => {
--- controllers/file_service/firebase_init.js
+++ controllers/file_service/firebase_init.js
@@ -1,35 +1,41 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import FirebaseStorage from "multer-firebase-storage";
+import { getApp } from "firebase/app";
 import Multer from "multer";
 import "dotenv/config";
+import fbAdmin from "firebase-admin";
 
 function firebaseInit() {
   // let fireBaseStorage = null;
   // TODO: Replace the following with your app's Firebase project configuration
   // See: https://firebase.google.com/docs/web/learn-more#config-object
+
+  const fbInstance = fbAdmin.initializeApp({
+    credential: fbAdmin.credential.cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      projectId: process.env.FIREBASE_PROJECT_ID
+    }),
+    storageBucket: "mapverse-e16e8.appspot.com"
+  })
+
   const multer = Multer({
     storage: FirebaseStorage({
       bucketName: process.env.FIREBASE_STORAGE_URL,
       directoryPath: 'mapverse_images',
-      credentials: {
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n") : undefined,
-        projectId: process.env.FIREBASE_PROJECT_ID
-      },
       public: true,
-      unique: true
-    })
+      unique: true,
+    }, fbInstance)
   })
 
   // Initialize Firebase
   // const firebaseApp = initializeApp(firebaseConfig);
-
-
   // // Initialize Cloud Storage and get a reference to the service
-  // fireBaseStorage = getStorage(firebaseApp);
+  // const fireBaseStorage = fbInstance.storage()
+  const fireBaseStorage = getStorage(fbInstance, process.env.FIREBASE_STORAGE_URL)
   // return fireBaseStorage;
-  return multer;
+  return { multer, fireBaseStorage };
 }
 
 export default firebaseInit;
